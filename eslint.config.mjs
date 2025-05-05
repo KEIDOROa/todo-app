c7f8a941c021aa04351655d6e404dc61630fbd3f
KEIDOROa <keidorpvp@icloud.com>
2025-05-05 23:31:17 +0900

    lintのエラーを修正

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,9 +3,32 @@ import globals from "globals";
 import pluginReact from "eslint-plugin-react";
 import { defineConfig } from "eslint/config";
 
-
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,jsx}"], plugins: { js }, extends: ["js/recommended"] },
-  { files: ["**/*.{js,mjs,cjs,jsx}"], languageOptions: { globals: globals.browser } },
+  {
+    files: ["**/*.{js,mjs,cjs,jsx}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        process: "readonly",
+      },
+    },
+  },
+  {
+    files: ["**/*.{js,mjs,cjs,jsx}"],
+    languageOptions: { globals: globals.browser },
+  },
+  {
+    files: ["**/*.test.js", "**/*.test.jsx"],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        jest: "readonly",
+        expect: "readonly",
+        test: "readonly",
+      },
+    },
+  },
   pluginReact.configs.flat.recommended,
 ]);
